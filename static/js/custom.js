bac896a80b023a1f42c9aa0cda4460f6cfeface0
Dave Quah <quahdave@gmail.com>
2018-03-18 02:31:42 +0800

    [Dave] clean up js and css files

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,3 @@
-let map;
 let origin;
 let destination;
 let placesArray;
@@ -6,7 +5,7 @@ let directionsService;
 let directionsDisplay;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 1.290270, lng: 103.851959 },
     zoom: 13,
     styles: [
@@ -22,25 +21,22 @@ function initMap() {
   directionsDisplay = new google.maps.DirectionsRenderer;
   directionsDisplay.setMap(map);
 
-  let locInput = document.getElementById('loc-input');
-  let autocomplete = new google.maps.places.Autocomplete(locInput);
+  const locInput = document.getElementById('loc-input');
+  const autocomplete = new google.maps.places.Autocomplete(locInput);
   autocomplete.addListener('place_changed', function () {
     origin = autocomplete.getPlace();
     if (!origin.geometry) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
       window.alert("No details available for input: '" + origin.name + "'");
       return;
     }
 
-    // Search for nearby restaurants based on location entered
     const searchRequest = {
       location: { lat: origin.geometry.location.lat(), lng: origin.geometry.location.lng() },
       radius: '750',
       type: ['restaurant']
     }
 
-    let service = new google.maps.places.PlacesService(map);
+    const service = new google.maps.places.PlacesService(map);
     service.nearbySearch(searchRequest, function (response, status) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
         placesArray = response;
@@ -61,12 +57,10 @@ function redrawRouteAndPopulate() {
   document.getElementById('dest-hours').innerHTML = destination.opening_hours ? 'Hours: Open' : 'Hours: Closed';
 }
 
-// Randomly pick and return a destination object from placesArray
 function randomizeDestination() {
   return placesArray[Math.floor(Math.random() * placesArray.length)];
 }
 
-// Draw route from origin to destination on map
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   directionsService.route({
     origin: origin.geometry.location,
